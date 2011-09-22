dd940e79adb9dd40b30e15ebcf13a59276bdf20d
hermzz <hermann.kaser@gmail.com>
2011-09-22 22:28:52 +0200

    AJAX calls to worker script with server param

--- index.php
+++ index.php
@@ -7,20 +7,51 @@ if(!isset($config['servers']))
 ?>
 <html>
 	<head>
-		<title>Deploy</title>
+		<title>Henchmen</title>
 		<script src="jquery.js"></script>
+		<script>
+			$(document).ready(function() 
+			{
+				$('#deploy_form').submit(function() 
+				{
+					$('.servers').each(function(i, e) {
+						$.ajax(
+							{
+								url: 'http://deploy.skynet/worker.php?server='+$(e).attr('name'),
+								success: show_ajax_results
+							}
+						);
+					});
+					
+					return false;
+				});
+				
+				$('input[name="all"]').click(function(e) {	
+					if(e.target.checked)
+					{
+						$('input[type="checkbox"]').attr('checked', true);
+					}
+				});
+			});
+			
+			function show_ajax_results(data, t, jqXHR) 
+			{
+				console.log(data);
+			}
+		</script>
 	</head>
 	<body>
 		<h2>Servers</h2>
-
+	
 		<ul>
+			<li><input type="checkbox" name="all" checked="checked"/>All</li>
 			<?php foreach($config['servers'] as $server): ?>
-				<li><?=$server;?></li>
+				<li><input type="checkbox" name="<?=$server;?>" checked="checked" class="servers" /><?=$server;?></li>
 			<?php endforeach; ?>
 		</ul>
-
-		<form action="#" method="post">
-			<input type="submit" name="submitDeploy" value="Deploy" />
+		
+		<form action="#" id="deploy_form" method="post">
+			<input type="submit" name="submit_deploy" value="Deploy" />
 		</form>
 	</body>
 </html>
---
+++ worker.php
@@ -0,0 +1,9 @@
+<?php 
+
+$sleep = rand(10,20);
+
+sleep($sleep);
+
+?>
+
+This is a worker for <?=$_GET['server'];?> and it slept for <?=$sleep;?> seconds!
